5ad6ec3167abb40ef211e7e1148a249e30aa4e32
Tom Bluher <tbluher14@gmail.com>
2022-08-06 14:59:58 -0600

    updated validation error handling for edit a review

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -32,10 +32,6 @@ module.exports = (sequelize, DataTypes) => {
     stars:{
       type: DataTypes.INTEGER,
       allowNull: false,
-      validate: {
-        min: 1,
-        max: 5
-      }
     },
   }, {
     sequelize,
