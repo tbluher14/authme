0de3cef65e8da5960f880ca077622d76eda8bb91
Tom Bluher <tbluher14@gmail.com>
2022-09-21 11:35:29 -0600

    updates for heroku

--- frontend/src/components/SignupFormPage/SignupForm.js
+++ frontend/src/components/SignupFormPage/SignupForm.js
@@ -23,7 +23,7 @@ function SignupForm() {
 
     const handleSubmit = async (e) => {
       e.preventDefault();
-      if (password === confirmPassword){
+      // if (password === confirmPassword){
         await dispatch(
           sessionActions.signup({
             firstName,
@@ -42,7 +42,7 @@ function SignupForm() {
             if (data?.message) setErrors([data.errors]);
           });
       }
-    }
+    // }
 
     return (
         <div className="signup_form_container">
