e963c0e07014149957f2668a3ec09e64b0b61b2c
Tom Bluher <tbluher14@gmail.com>
2022-09-22 20:20:34 -0600

    added seed

--- backend/db/seeders/20220916215738-demo-image.js
+++ backend/db/seeders/20220916215738-demo-image.js
@@ -60,59 +60,149 @@ module.exports = {
         previewImage: true
         },
         {
-          url: 'https://tinyurl.com/27y2bnce',
+        url: 'https://tinyurl.com/27y2bnce',
+        reviewId: 3,
+        spotId: 5,
+        imageableId: 1,
+        imageableType: 'Review',
+        userId: 1,
+        previewImage: true
+          },
+          {
+        url: 'https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        reviewId: 3,
+        spotId: 6,
+        imageableId: 1,
+        imageableType: 'Review',
+        userId: 1,
+        previewImage: true
+            },
+            {
+        url: 'https://images.unsplash.com/photo-1626684496076-07e23c6361ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=834&q=80',
+        reviewId: 3,
+        spotId: 7,
+        imageableId: 1,
+        imageableType: 'Review',
+        userId: 1,
+        previewImage: true
+      },
+       {
+        url: 'https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        reviewId: 3,
+        spotId: 8,
+        imageableId: 1,
+        imageableType: 'Review',
+        userId: 1,
+        previewImage: true
+       },
+        {
+        url: 'http://southparkstudios.mtvnimages.com/shared/locations/restaurant-sodosopa.png',
+        reviewId: 3,
+        spotId: 9,
+        imageableId: 1,
+        imageableType: 'Review',
+        userId: 1,
+        previewImage: true
+        },
+        {
+        url: 'https://images.unsplash.com/photo-1570793005386-840846445fed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80',
+        reviewId: 3,
+        spotId: 10,
+        imageableId: 1,
+        imageableType: 'Review',
+        userId: 1,
+        previewImage: true
+        },
+        {
+          url: 'https://images.unsplash.com/photo-1518107784960-eb57c673a7ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
           reviewId: 3,
-          spotId: 5,
+          spotId: 11,
           imageableId: 1,
           imageableType: 'Review',
           userId: 1,
           previewImage: true
           },
           {
-            url: 'https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+            url: 'https://images.unsplash.com/photo-1449158743715-0a90ebb6d2d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
             reviewId: 3,
-            spotId: 6,
+            spotId: 12,
             imageableId: 1,
             imageableType: 'Review',
             userId: 1,
             previewImage: true
             },
             {
-              url: 'https://images.unsplash.com/photo-1626684496076-07e23c6361ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=834&q=80',
+              url: 'https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
               reviewId: 3,
-              spotId: 7,
+              spotId: 13,
               imageableId: 1,
               imageableType: 'Review',
               userId: 1,
               previewImage: true
               },
               {
-                url: 'https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+                url: 'https://images.unsplash.com/photo-1522043436628-a4bd7867030b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80',
                 reviewId: 3,
-                spotId: 8,
+                spotId: 14,
                 imageableId: 1,
                 imageableType: 'Review',
                 userId: 1,
                 previewImage: true
                 },
                 {
-                  url: 'http://southparkstudios.mtvnimages.com/shared/locations/restaurant-sodosopa.png',
+                  url: 'https://images.unsplash.com/photo-1485038101637-2d4833df1b35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80',
                   reviewId: 3,
-                  spotId: 9,
+                  spotId: 15,
                   imageableId: 1,
                   imageableType: 'Review',
                   userId: 1,
                   previewImage: true
                   },
                   {
-                    url: 'https://images.unsplash.com/photo-1570793005386-840846445fed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80',
+                    url: 'https://images.unsplash.com/photo-1475087542963-13ab5e611954?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
                     reviewId: 3,
-                    spotId: 10,
+                    spotId: 16,
                     imageableId: 1,
                     imageableType: 'Review',
                     userId: 1,
                     previewImage: true
                     },
+                    {
+                      url: 'https://images.unsplash.com/photo-1522360262400-9ae2dcfd8cdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+                      reviewId: 3,
+                      spotId: 17,
+                      imageableId: 1,
+                      imageableType: 'Review',
+                      userId: 1,
+                      previewImage: true
+                      },
+                      {
+                        url: 'https://images.unsplash.com/photo-1521898208365-d7bd8e1ad280?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=713&q=80',
+                        reviewId: 3,
+                        spotId: 18,
+                        imageableId: 1,
+                        imageableType: 'Review',
+                        userId: 1,
+                        previewImage: true
+                        },
+                        {
+                          url: 'https://images.unsplash.com/photo-1542718610-a1d656d1884c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+                          reviewId: 3,
+                          spotId: 19,
+                          imageableId: 1,
+                          imageableType: 'Review',
+                          userId: 1,
+                          previewImage: true
+                          },
+                          {
+                            url: 'https://images.unsplash.com/photo-1487148742994-2f29d489182f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80',
+                            reviewId: 3,
+                            spotId: 20,
+                            imageableId: 1,
+                            imageableType: 'Review',
+                            userId: 1,
+                            previewImage: true
+                            },
     ]
    )
   },
