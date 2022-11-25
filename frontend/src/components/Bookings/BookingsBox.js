eba2384ff64de206c90a4382519b523754b71213
Tom Bluher <tbluher14@gmail.com>
2022-11-25 12:26:27 -0700

    updates for heroku

--- frontend/src/components/Bookings/BookingsBox.js
+++ frontend/src/components/Bookings/BookingsBox.js
@@ -2,7 +2,6 @@ import {useParams, useHistory} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import { useState } from 'react';
 import { createBookingThunk, findAllBookingsThunk } from '../../store/bookings';
-import {format} from 'date-fns'
 import  LoginFormModal  from '../LoginFormModal/index';
 import { useEffect } from 'react';
 import { listAllSpots } from '../../store/spot';
